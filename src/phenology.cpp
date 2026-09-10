26796dfd1dfae93f6fa7f4ef98db61dc2afcb137
Eduardo Lacerda <eduardolacerdageo@gmail.com>
2026-09-10 17:03:38 +0200

    fix(build): add missing OpenMP dummies to phenology.cpp

--- src/phenology.cpp
+++ src/phenology.cpp
@@ -1,4 +1,14 @@
 #include "phenology.h"
+
+#ifndef _OPENMP
+#ifndef OMP_DUMMIES_DEFINED
+#define OMP_DUMMIES_DEFINED
+#define omp_get_max_threads() 1
+#define omp_get_thread_num() 0
+#define omp_set_num_threads(x) (void)(x)
+#endif
+#endif
+
 #ifdef _OPENMP
 #include <omp.h>
 #endif
