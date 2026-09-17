56f8c825e2a73fc645da8126c8bb0522007dd83c
Eduardo Lacerda <eduardolacerdageo@gmail.com>
2026-09-17 14:41:10 +0200

    chore: bump version to 0.9.0
    Breaking change in cdts.ai.TempCNN (previous commit) warrants a minor
    version bump under 0.x semver.

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ class BuildExt(build_ext):
 
 setup(
     name='cdts',
-    version='0.8.0',
+    version='0.9.0',
     packages=['cdts'],
     ext_modules=ext_modules,
     setup_requires=['pybind11>=2.10.0'],
