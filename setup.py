75f6e2d470863f67ff127bc5b1b696d3740960a4
Eduardo Lacerda <eduardolacerdageo@gmail.com>
2026-09-24 21:23:42 +0200

    chore: bump version to 0.20.0

--- setup.py
+++ setup.py
@@ -91,7 +91,7 @@ class BuildExt(build_ext):
 
 setup(
     name='cdts',
-    version='0.19.0',
+    version='0.20.0',
     packages=['cdts'],
     ext_modules=ext_modules,
     setup_requires=['pybind11>=2.10.0'],
